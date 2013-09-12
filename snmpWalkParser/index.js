c847d3213db6ca7bbd04669ddba3784b7c881adf
isszul <Isszul@gmail.com>
2013-09-12 21:21:35 +0100

    updated parser to pull out data type and data value into dict

--- snmpWalkParser/index.js
+++ snmpWalkParser/index.js
@@ -7,15 +7,25 @@ function SnmpWalkParser(nosql) {
 };
 
 
-SnmpWalkParser.prototype.processSnmpWalkFile = function(filename) {
+SnmpWalkParser.prototype.processSnmpWalkFile = function (filename) {
 
-    fs.readFile(filename, "utf8", function(err, snmpWalkString) {
+    fs.readFile(filename, "utf8", function (err, fileAsString) {
 
         if (err) throw err;
 
-        snmpWalkString.split("\n").map(function(value, key) {
+        fileAsString.split("\r\n").map(function (line) {
 
-            Nosql.insert({ 'value': value, 'key': key });
+            line = line.split(" = ");
+
+            key = line[0];
+
+            value = line[1].split(": ");
+            dataType = value[0];
+            dataValue = value[1];
+
+
+            Nosql.insert({ 'key': key, 'dataType': dataType, 'dataValue': dataValue });
+
 
         });
 
