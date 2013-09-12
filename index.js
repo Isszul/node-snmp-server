fb90d35a825db7beddebf45b0b27c54d9a107740
isszul <Isszul@gmail.com>
2013-09-13 00:12:30 +0100

    Replaced packet parsed with snmp lib and updated flow so real snmp oids are looked up in the nosql db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var SNMP_PORT = 161;
 
 var nosql = require('nosql').load("./tmpDB.nosql");
 var snmpWalkParser = require('./snmpWalkParser').init();
-var snmpSocketListener = require('./snmpSocketListener').init(SNMP_PORT);
+
 
 
 nosql.clear();
@@ -14,10 +14,6 @@ snmpWalkParser.processSnmpWalkFile("./example.snmpwalk", nosql);
 
 snmpWalkParser.on("fileprocessed", function () {
 
-    nosql.all("doc.oid == '.1.3.6.1.4.1.5528.100.4.1.10.1.5.1382714834'", function (doc, offset) {
+    var snmpSocketListener = require('./snmpSocketListener').init(SNMP_PORT, nosql);
 
-        console.log(doc);
-
-    });
 });
-
--- snmpSocketListener/index.js
+++ snmpSocketListener/index.js
@@ -1,12 +1,27 @@
 
-
 var events = require('events');
 var dgram = require('dgram');
-var snmpPacketParser = require('../snmpPacketParser');
+var asn1ber = require('../lib/asn1ber.js');
+var snmp = require('../lib/snmp.js');
+
+function convertOID(oid) {
+
+    var output = ".";
+
+    for (var i=0; i<oid.length; i++) {
+        output = output + oid[i];
+        if (i<oid.length-1) {
+            output = output + "."
+        }
+    }
+    return output;
+
+}
 
 
-function SnmpSocketListener(port) {
+function SnmpSocketListener(port, nosql) {
 
+    Nosql = nosql;
     this.socket = dgram.createSocket('udp4');
     this.socket.bind(port);
 
@@ -18,15 +33,16 @@ SnmpSocketListener.prototype = new events.EventEmitter();
 
 SnmpSocketListener.prototype.messageRecieved = function (msg, rinfo) {
 
-    console.log(msg);
-    parsedPacket = snmpPacketParser.parsePacket(msg);
+    var oid = convertOID(snmp.parse(msg)['pdu']['varbinds'][0]['oid']);
 
-    console.log(parsedPacket["OBJECT_IDENTIFIER_VALUE"]);
+    Nosql.all("doc.oid == '"+oid+"'", function (doc, offset) {
 
+        console.log(doc);
 
+    });
 }
 
 
-exports.init = function (port) {
-    return new SnmpSocketListener(port);
+exports.init = function (port, nosql) {
+    return new SnmpSocketListener(port, nosql);
 }
