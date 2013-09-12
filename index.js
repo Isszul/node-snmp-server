b7fde7907733cb1ae3b81a3324aa2adfbb06a5b3
isszul <Isszul@gmail.com>
2013-09-13 00:50:14 +0100

    tidied up imports in index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var SNMP_PORT = 161;
 
+var snmpSocketListener = require('./snmpSocketListener');
 var nosql = require('nosql').load("./tmpDB.nosql");
+var snmpWalkParser = require('./snmpWalkParser').init();
+
+
 nosql.clear();
 
-var snmpWalkParser = require('./snmpWalkParser').init();
 snmpWalkParser.processSnmpWalkFile("./example.snmpwalk", nosql);
 
 snmpWalkParser.on("fileprocessed", function () {
 
-    var snmpSocketListener = require('./snmpSocketListener').init(SNMP_PORT, nosql);
+    snmpSocketListener.init(SNMP_PORT, nosql);
 
 });
